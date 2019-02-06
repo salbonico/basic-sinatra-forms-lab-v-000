8303cd2568b0c1f09d6c9246e237ff3521b248d5
Steve Albonico <salbonico@gmail.com>
2019-02-06 00:13:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,11 +9,11 @@ end
 post '/team' do
 @coach = params[:coach]
 @team_name = params[:name]
-@point_guard = params[:point_guard]
-@shooting_guard = params[:shooting_guard]
-@small_forward = params[:small_forward]
-@power_forward = params[:power_forward]
-@center = params[:center]
+@point_guard = params[:pg]
+@shooting_guard = params[:sg]
+@small_forward = params[:sf]
+@power_forward = params[:pf]
+@center = params[:c]
 erb :team
 end
 
